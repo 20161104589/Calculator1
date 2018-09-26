9fdec69631e7efcc309396280fe17bb3d408eb3b
20161104589 <a20161104589@admindeiMac-2.local>
2018-09-26 12:00:18 +0800

    设计了计算器界面

---
+++ Calculator1/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by 20161104589 on 2018/9/26.
+//  Copyright © 2018年 20161104589. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func button1(_ sender: Any) {
+    }
+    @IBAction func button2(_ sender: Any) {
+    }
+    @IBAction func button3(_ sender: Any) {
+    }
+    @IBAction func button4(_ sender: Any) {
+    }
+    @IBAction func button5(_ sender: Any) {
+    }
+    @IBAction func button6(_ sender: Any) {
+    }
+    @IBAction func button7(_ sender: Any) {
+    }
+    @IBAction func button8(_ sender: Any) {
+    }
+    @IBAction func button9(_ sender: Any) {
+    }
+    @IBAction func button0(_ sender: Any) {
+    }
+    @IBAction func button_dot(_ sender: Any) {
+    }
+    @IBAction func button_add(_ sender: Any) {
+    }
+    @IBAction func button_minus(_ sender: Any) {
+    }
+    @IBAction func button_ride(_ sender: Any) {
+    }
+    @IBAction func button_eli(_ sender: Any) {
+    }
+    @IBAction func button_equal(_ sender: Any) {
+    }
+    @IBAction func button_zero(_ sender: Any) {
+    }
+    @IBAction func button_square(_ sender: Any) {
+    }
+    @IBAction func button_percent(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
