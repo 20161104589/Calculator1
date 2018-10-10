4f379ece77514fb4aa4e649ba6a3426428a3d09e
20161104589 <a20161104589@admindeiMac-2.local>
2018-10-10 11:54:56 +0800

    实现了加减乘除

--- Calculator1/ViewController.swift
+++ Calculator1/ViewController.swift
@@ -11,6 +11,11 @@ var control:Int = 0
 var number_one:String = ""
 class ViewController: UIViewController {
     var DisplayWindowAcknowledgement = false
+    var n = 0.0
+    var m = 0.0
+    var sum=0.0
+    var fag:String = ""
+    @IBOutlet weak var num: UITextField!
     @IBAction func button1(_ sender: Any) {
         let button1=(sender as AnyObject).currentTitle
         if(DisplayWindowAcknowledgement){
@@ -23,25 +28,50 @@ class ViewController: UIViewController {
             }
         }
     }
-    @IBAction func button_dot(_ sender: Any) {
-    }
     @IBAction func button_add(_ sender: Any) {
+        n = Double(num.text!)!
+        num.text=""
+        fag = "a"
     }
     @IBAction func button_minus(_ sender: Any) {
+        n = Double(num.text!)!
+        num.text=""
+        fag = "m"
     }
     @IBAction func button_ride(_ sender: Any) {
+        n = Double(num.text!)!
+        num.text=""
+        fag = "r"
     }
     @IBAction func button_eli(_ sender: Any) {
+        n = Double(num.text!)!
+        num.text=""
+        fag = "e"
     }
     @IBAction func button_equal(_ sender: Any) {
+        m = Double(num.text!)!
+        switch fag {
+        case "a" :   sum = n + m
+            break
+        case "m" :   sum = n - m
+            break
+        case "r" :   sum = n * m
+            break
+        case "e" :   sum = n / m
+            break
+        default:
+            break
+        }
+        var display = String(sum)
+        num.text=display
     }
     @IBAction func button_zero(_ sender: Any) {
+        num.text=""
     }
     @IBAction func button_square(_ sender: Any) {
     }
     @IBAction func button_percent(_ sender: Any) {
     }
-    @IBOutlet weak var num: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
